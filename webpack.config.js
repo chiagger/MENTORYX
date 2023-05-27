2e2a8dec90a2bacdd6e17d77d8b1551d08ba05b4
chiagger <busca.chiara.cb@gmail.com>
2023-05-27 19:34:54 +0200

    Webpack setup and index.html created dynamically

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/landingpage/index.js',
+    auth: './src/auth/auth.js',
+  },
   mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'MENTORYX',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
