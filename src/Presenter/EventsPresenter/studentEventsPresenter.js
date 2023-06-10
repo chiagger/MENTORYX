28d915e95464876c9dbb6dcd75211443695d6895
chiagger <busca.chiara.cb@gmail.com>
2023-06-10 20:07:19 +0200

    fixed student drop down menu

--- src/Presenter/EventsPresenter/studentEventsPresenter.js
+++ src/Presenter/EventsPresenter/studentEventsPresenter.js
@@ -10,52 +10,6 @@ import '../../View/homeStudente/homeStudente.css';
 import LogPresenter from "../LogPresenter/LogPresenter.js";
 const log = new LogPresenter();
 
-// Mostra o nasconde il dropdown del profilo
-document.getElementById("profile-icon").addEventListener("click", function () {
-    const profileDropdown = document.getElementById("profile-dropdown");
-    profileDropdown.style.display = (profileDropdown.style.display === "block") ? "none" : "block";
-});
-
-// Aggiunta effetti visivi dinamici e responsivi
-window.addEventListener("mousemove", function (e) {
-    const balls = document.querySelectorAll(".ball");
-    const mouseX = e.pageX;
-    const mouseY = e.pageY;
-
-    balls.forEach(function (ball) {
-        const ballX = ball.getBoundingClientRect().left + ball.clientWidth / 2;
-        const ballY = ball.getBoundingClientRect().top + ball.clientHeight / 2;
-
-        const diffX = mouseX - ballX;
-        const diffY = mouseY - ballY;
-
-        const distance = Math.sqrt(diffX * diffX + diffY * diffY);
-
-        const maxDistance = Math.max(window.innerWidth, window.innerHeight) / 2;
-
-        const scale = 1 - distance / maxDistance;
-
-        ball.style.transform = `scale(${scale})`;
-    });
-});
-
-
-function getUserCategory(uid) {
-    const myUserData = ref(db);
-    return get(child(myUserData, "Users/" + uid))
-        .then((snapshot) => {
-            const snapshotValue = snapshot.val();
-            const childKeys = Object.keys(snapshotValue);
-            const category = childKeys[0];
-            return category;
-        })
-        .catch((error) => {
-            alert(error);
-            throw error; // Propagate the error further
-        });
-}
-
-
 
 //ricerca ascoltatori
 const emptyContainer = document.getElementById("emptyContainer");
